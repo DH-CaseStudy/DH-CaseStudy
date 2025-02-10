fe02de2318a01dea9efa51907307412e030ae0a7
Donghwi <cksael@gmail.com>
2025-02-10 23:39:15 +0900

    DTO getter / setter 정의

--- Study/src/StudentManagementSystem/dto/StudentDTO.java
+++ Study/src/StudentManagementSystem/dto/StudentDTO.java
@@ -7,19 +7,91 @@ public class StudentDTO {
     private int english;
     private int math;
     private int science;
-    private int totla;
+    private int total;
     private float average;
     private String grade;
 
-    public StudentDTO(String sno, String name, int korean, int english, int math, int science, int totla, float average, String grade) {
+    public StudentDTO(String sno, String name, int korean, int english, int math, int science, int total, float average, String grade) {
         this.sno = sno;
         this.name = name;
         this.korean = korean;
         this.english = english;
         this.math = math;
         this.science = science;
-        this.totla = totla;
+        this.total = total;
         this.average = average;
+        this.grade = grade;
+    }
+
+    public String getSno() {
+        return sno;
+    }
+
+    public void setSno(String sno) {
+        this.sno = sno;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getKorean() {
+        return korean;
+    }
+
+    public void setKorean(int korean) {
+        this.korean = korean;
+    }
+
+    public int getEnglish() {
+        return english;
+    }
+
+    public void setEnglish(int english) {
+        this.english = english;
+    }
+
+    public int getMath() {
+        return math;
+    }
+
+    public void setMath(int math) {
+        this.math = math;
+    }
+
+    public int getScience() {
+        return science;
+    }
+
+    public void setScience(int science) {
+        this.science = science;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+    public float getAverage() {
+        return average;
+    }
+
+    public void setAverage(float average) {
+        this.average = average;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
         this.grade = grade;
     }
 }
